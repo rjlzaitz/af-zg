4ce14e2c619e5fd99c256888f0c52de87e548dc0
Zach Schneider <me@zachschneider.com>
2013-03-30 19:29:11 -0500

    added some ie support

--- www/header.php
+++ www/header.php
@@ -36,14 +36,19 @@ $ssn = $profile['ssn'];
 $location = $profile['location'];
 $email = $profile['email'];
 ?>
-<!DOCTYPE html>
-<html lang="en-US">
+
+<!doctype html>
+<!--[if lt IE 7]> <html class="no-js ie ie6 lt-ie10 lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js ie ie7 lt-ie10 lt-ie9 lt-ie8" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js ie ie8 lt-ie10 lt-ie9" lang="en"> <![endif]-->
+<!--[if IE 9]>    <html class="no-js ie ie9 lt-ie10" lang="en"> <![endif]-->
+<!--[if gt IE 9]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
 	<head>
-		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0;" />
 		<title>Z&#243;calo Group Domain Holding Service</title>
-		<link rel="stylesheet" type="text/css" href="./style.css">
-		<link href='http://fonts.googleapis.com/css?family=Arvo:400,700|Open+Sans:400,700' rel='stylesheet' type='text/css'>
+		<link rel="stylesheet" type="text/css" href="./style.css" />
+		<link href='http://fonts.googleapis.com/css?family=Arvo:400,700|Open+Sans:400,700' rel='stylesheet' type='text/css' /> 
 		<script src="./js/modernizr.js"></script>
 	</head>
 	<body>
